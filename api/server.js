c884f260afe04df3443615cf5938884ee378e786
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-31 08:46:45 -0400

    express server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h2>Hello from express 🤖</h2>
+    `)
+});
+
+module.exports = server;
