f780b2c98e5d17f944d5472735b55ec081d95774
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-08-01 00:53:59 -0400

    api

--- api/server.js
+++ api/server.js
@@ -6,9 +6,17 @@ const tasksRouter = require('./task/router');
 const server = express();
 
 server.use(express.json());
+
 server.use('/api/resources', resourcesRouter);
 server.use('/api/projects', projectsRouter);
 server.use('/api/tasks', tasksRouter);
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.essage,
+        stack: err.stack,
+    });
+});
 
 server.get('/', (req, res) => {
     res.send(`
