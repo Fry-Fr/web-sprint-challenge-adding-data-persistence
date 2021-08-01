1ed4f00778de9b4fe520d7319d9853d0f3ff72f3
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-08-01 00:50:26 -0400

    database queries

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,15 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    createTask,
+    getTasks,
+};
+
+async function createTask(post) {
+    const [id] = await db('tasks').insert(post);
+    return db('tasks').where({ "task_id": id }).first();
+}
+
+function getTasks() {
+    return db('tasks');
+}
