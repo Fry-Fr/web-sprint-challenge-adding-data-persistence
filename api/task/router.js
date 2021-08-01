0ffdf7224dc471c24cedd19d8c769272e34680b9
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-08-01 00:51:15 -0400

    tasks endpoints

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,27 @@
-// build your `/api/tasks` router here
+const router = require('express').Router();
+const Model = require('./model');
+
+router.post('/', (req, res, next) => {
+    const newTask = req.body;
+    Model.createTask(newTask)
+        .then(newPost => {
+            if (newPost.task_completed === 0) {
+                newPost.task_completed = false;
+                res.status(201).json(newPost);
+            }else {
+                newPost.task_completed = true;
+                res.status(201).json(newPost);
+            }
+        })
+        .catch(err => next(err))
+});
+
+router.get('/', (req, res, next) => {
+    Model.getTasks()
+        .then(tasks => {
+            res.json(tasks)
+        })
+        .catch(err => next(err))
+});
+
+module.exports = router;
