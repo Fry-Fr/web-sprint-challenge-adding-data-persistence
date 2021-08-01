e8ce5c84988e13c316f89e9fd85d1eb1897101c0
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-08-01 00:28:50 -0400

    projects endpoints

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,27 @@
-// build your `/api/projects` router here
+const router = require('express').Router();
+const Model = require('./model');
+
+router.post('/', (req, res, next) => {
+    const newProject = req.body;
+    Model.createProject(newProject)
+        .then(newPost => {
+            if (newPost.project_completed === 0) {
+                newPost.project_completed = false;
+                res.status(201).json(newPost)
+            }else {
+                newPost.project_completed = true;
+                res.status(201).json(newPost)
+            }
+        })
+        .catch(err => next(err))
+});
+
+router.get('/', (req, res, next) => {
+    Model.getProjects()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(err => next(err))
+});
+
+module.exports = router;
